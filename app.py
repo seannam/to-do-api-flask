247cd3090dd8f13fdf04a23d0c2bdd9c19da17e3
Sean Nam <namsangwoo1@gmail.com>
2017-02-17 02:29:32 -0800

    Added public make_task method and added security with login using httpauth

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-#!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask.ext.httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
 
 app = Flask(__name__)
 
@@ -18,21 +19,86 @@ tasks = [
     }
 ]
 
+def make_public_task(task):
+	new_task = {}
+	for field in task:
+		if field == 'id':
+			new_task['url'] = url_for('get_task', task_id=task['id'], _external=True)
+		else:
+			new_task[field] = task[field]
+	return new_task
 
 @app.route('/')
 def home():
 	return "To Do API Home Page " + '/todo/api/v1.0/tasks' 
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
 def get_tasks():
-	return jsonify({'tasks': tasks})
+	#return jsonify({'tasks': tasks})
+	return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
 @app.route('/todo/api/v1.0/tasks<int:task_id>', methods=['GET'])
 def get_task(task_id):
 	task = [task for task in tasks if task['id'] == task_id]
 	if len(task) == 0:
 		abort(404)
+	return jsonify({'task': [make_public_task(task[0])]})
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+	if not request.json or not 'title' in request.json:
+		abort(400)
+	task = {
+		'id': tasks[-1]['id'] + 1,
+		'title': request.json['title'],
+		'description': request.json.get('description', ""),
+		'done': False
+	}
+	tasks.append(task)
+	return jsonify({'task': task}), 201
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+	task = [task for task in tasks if task['id'] == task_id]
+	if len(task) == 0:
+		abort(404)
+	if not request.json:
+		abort(400)
+	if 'title' in request.json and type(request.json['title']) != unicode:
+		abort(400)
+	if 'done' in request.json and type(request.json['done']) is not bool:
+		abort(400)
+
+	task[0]['title'] = request.json.get('title', task[0]['title'])
+	task[0]['description'] = request.json.get('description', task[0]['description'])
+	task[0]['done'] = request.json.get('done', task[0]['description'])
 	return jsonify({'task': task[0]})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+	task = [task for task in tasks if task['id'] == task_id]
+	if len(task) == 0:
+		abort(404)
+	task.remove(tasks[0])
+	return jsonify({'result': True})
+
+# Error page
+@app.errorhandler(404)
+def no_found(error):
+	return make_response(jsonify({'error': 'Not found'}), 404)
+
+# Login
+
+@auth.get_password
+def get_password(username):
+	if username == 'mike':
+		return 'python'
+	return None
+
+@auth.error_handler
+def unauthorized():
+	return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 if __name__ == '__main__':
 	app.run("127.0.0.1", 8080, debug=True);
